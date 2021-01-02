8598ee09eef80e8a12920f58315b4c745bcdeadb
DreamWasTaken28 <76871433+DreamWasTaken28@users.noreply.github.com>
2021-01-02 18:26:37 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+var SpeechRecognition = window.webkitSpeechRecognition;
+var recongnition = new SpeechRecognition();
+function start()
+{
+    document.getElementById("textbox").innerHTML = "";
+    recongnition.start();
+}
+recongnition.onresult = function run(event)
+{
+    console.log(event);
+    var Content = event.results[0][0].transcript;
+    console.log(Content);
+    document.getElementById("textbox").innerHTML = Content;
+}
